871adf2f20251acc351ca9d8dae58566c25ab99a
DESKTOP-4QAGBRG\Administrator <727123417@qq.com>
2025-05-11 00:23:52 +0800

    增加测试粒子

--- java/com/charge/chargemod/item/sword/EmberSword.java
+++ java/com/charge/chargemod/item/sword/EmberSword.java
@@ -7,12 +7,16 @@ import com.charge.chargemod.network.ChargePacketSender;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.Random;
 
 public class EmberSword extends ChargeBaseSword{
     @Override
@@ -32,16 +36,22 @@ public class EmberSword extends ChargeBaseSword{
                 if (entity.getRemainingFireTicks() > 0) {
                     DamageSource damageSource = DaoFaDamageSource.source(user, ChargeDamageTypes.DAO_REAL);
                     //第三个参数是计算伤害的东西
-                    level.explode(user,damageSource,null,entity.getEyePosition(),4.f,false,Level.ExplosionInteraction.NONE);
-                    entity.hurt(DaoFaDamageSource.source(user,ChargeDamageTypes.DAO_REAL), 10);
+                    level.explode(user, damageSource, null, entity.getEyePosition(), 4.f, false, Level.ExplosionInteraction.NONE);
+                    entity.hurt(DaoFaDamageSource.source(user, ChargeDamageTypes.DAO_REAL), 10);
                 } else {
                     entity.hurt(user.level().damageSources().lava(), 10);
                     entity.setSecondsOnFire(8);
 
                 }
-                return true;
+            } else {
+                Random random = new Random();
+                for (int i = 0; i < 8; i++) {
+                    Vec3 center = entity.getEyePosition();
+                    Vec3 to_vec = new Vec3(random.nextFloat(2)-1,random.nextFloat(2)-1,random.nextFloat(2)-1).normalize().scale(0.1);
+                    level.addParticle(ParticleTypes.FLAME, center.x, center.y, center.z , to_vec.x, to_vec.y, to_vec.z);
+                }
             }
-
+            return true;
         }
         return false;
 
